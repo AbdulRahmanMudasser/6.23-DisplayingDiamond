e03e46c1672e8da299a718a39c6ce4379c91221e
Abdul Rahman <abdulrahmanmuddasser@gmail.com>
2023-05-01 11:31:32 +0500

    testing

--- Displaying Diamond/Program.cs
+++ Displaying Diamond/Program.cs
@@ -1,2 +1,36 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿class DisplayingDiamonds
+{
+    static void Main(string[] args)
+    {
+        // declaring variables
+        int n = 5;
+        
+        // upper half of diamond
+        for (int i = 1; i <= n; i++)
+        {
+            for (int j = 1; j <= n - i; j++)
+            {
+                Console.Write(" ");
+            }
+            for (int j = 1; j <= 2 * i - 1; j++)
+            {
+                Console.Write("*");
+            }
+            Console.WriteLine();
+        }
+
+        // lower half of diamond
+        for (int i = n - 1; i >= 1; i--)
+        {
+            for (int j = 1; j <= n - i; j++)
+            {
+                Console.Write(" ");
+            }
+            for (int j = 1; j <= 2 * i - 1; j++)
+            {
+                Console.Write("*");
+            }
+            Console.WriteLine();
+        }
+    }
+}
